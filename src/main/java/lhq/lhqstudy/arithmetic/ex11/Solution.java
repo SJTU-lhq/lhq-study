757138fe2c1a6b32491326a0a3fb0a91c0520938
lihaiquan <964939913@qq.com>
2023-12-19 22:04:12 +0800

    23.1219

--- src/main/java/lhq/lhqstudy/arithmetic/ex11/Solution.java
+++ src/main/java/lhq/lhqstudy/arithmetic/ex11/Solution.java
@@ -6,12 +6,21 @@ package lhq.lhqstudy.arithmetic.ex11;
  * @date 2023/12/18 15:47
  */
 public class Solution {
-    public int maxArea(int[] height) {
+/*    public int maxArea(int[] height) {
         int maxArea = 0;
         for (int i = 0; i < height.length; i++) {
             for (int j = 0; j < i; j++) {
                 maxArea = Math.max(maxArea, (i - j) * Math.min(height[i], height[j]));
             }
+        }
+        return maxArea;
+    }*/
+
+    public int maxArea(int[] height) {
+        int maxArea = 0, i = 0, j = height.length-1;
+        while(i<j){
+            int minHeight = height[i] < height[j]?height[i++]:height[j--];
+            maxArea = Math.max(maxArea, (j-i + 1)*minHeight);
         }
         return maxArea;
     }
